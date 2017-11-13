f4dd1100cda48cbbc76b701f941cc3cd087923f6
Albert Richard Sanyoto <albertalrisa@gmail.com>
2017-11-13 17:20:06 +0700

    Add updated LSTM Layer
    The LSTM layer in the previous commits are based on the TrainingCSharp
    example in CNTK repository. This commit adds rewritten LSTM layer based
    on the Python implementation.
    
    The new model should behave identically to the Python version of CNTK.
    
    Signed-off-by: Albert Richard Sanyoto <albertalrisa@gmail.com>

--- playground/CNTKRefresh/CharRNNCNTK/LSTM.cs
+++ playground/CNTKRefresh/CharRNNCNTK/LSTM.cs
@@ -10,10 +10,8 @@ namespace CharRNNCNTK
     /// <summary>
     /// This class builds an LSTM layer and returns it as a Function
     /// 
-    /// Adapted from CNTK C# Training Example for LSTM Sequence Classifier
-    /// https://github.com/Microsoft/CNTK/blob/master/Examples/TrainingCSharp/Common/LSTMSequenceClassifier.cs
-    /// Implemented according to the formula on Colah's Blog on Understanding LSTM
-    /// http://colah.github.io/posts/2015-08-Understanding-LSTMs/
+    /// Adapted from LSTM Block in Python implementation
+    /// https://github.com/Microsoft/CNTK/blob/master/bindings/python/cntk/layers/blocks.py
     /// </summary>
     class LSTM
     {
@@ -37,8 +35,7 @@ namespace CharRNNCNTK
             Function lstmFunction = LSTMComponent<T>(input, new int[] {lstmDimension}, new int[] {cellDimension},
                     pastValueRecurrenceHook, pastValueRecurrenceHook, enableSelfStabilization, device)
                 .Item1;
-            return Function.AsComposite(lstmFunction, outputName);
-            //return CNTKLib.SequenceLast(lstmFunction, outputName);
+            return Function.Alias(lstmFunction, "LSTM");
         }
 
         public static Function Build(Variable input,int lstmDimension, DeviceDescriptor device,
@@ -61,177 +58,54 @@ namespace CharRNNCNTK
         public static Tuple<Function, Function> LSTMCell<TElementType>(Variable input, Variable prevOutput,
             Variable prevCellState, bool enableSelfStabilization, DeviceDescriptor device)
         {
+            //  TODO: Implements Self Stabilization
+            //  TODO: Implements Peephole
             int lstmOutputDimension = prevOutput.Shape[0];
             int lstmCellDimension = prevCellState.Shape[0];
 
             bool isFloatType = typeof(TElementType) == typeof(float);
             DataType dataType = isFloatType ? DataType.Float : DataType.Double;
 
-            //  This is done according to the example in CNTK. The problem with this is that because it is called multiple times,
-            //  the number of parameter tensor increases. Fix may be needed to make it identical to the one on Python.
-            Func<int, Parameter> createBiasParameters;
-            if(isFloatType) createBiasParameters = (dimension) => new Parameter(new [] {dimension}, 0.01f, device, "Bias");
-            else createBiasParameters = (dimension) => new Parameter(new[] { dimension }, 0.01, device, "Bias");
-
             uint seed = 1;
-            Func<int, Parameter> createWeightParameters = (outputDimension) => new Parameter(new [] { outputDimension, NDShape.InferredDimension }, 
-                dataType, CNTKLib.GlorotUniformInitializer(1.0, 1, 0, seed++), device, "Weight");
+            Parameter W = new Parameter((NDShape) new[] { lstmCellDimension * 4, NDShape.InferredDimension }, dataType, 
+                CNTKLib.GlorotUniformInitializer(1.0, 1, 0, seed++), device, "W");
+            Parameter b = new Parameter((NDShape) new[] { lstmCellDimension * 4 }, dataType, CNTKLib.GlorotUniformInitializer(1.0, 1, 0, seed++), device, "b");
 
-            Function stabilizedPrevOutput;
-            if (enableSelfStabilization)
-                stabilizedPrevOutput = Stabilize.Build<TElementType>(prevOutput, device, "StabilizedPrevOutput");
-            else
-                stabilizedPrevOutput = prevOutput;
+            Variable linearCombination = CNTKLib.Times(W, input, "linearCombinationInput");
+            Variable linearCombinationPlusBias = CNTKLib.Plus(b, linearCombination, "linearCombinationInputPlusBias");
 
-            Func<Variable> InputLinearCombinationPlusBias = () =>
-                CNTKLib.Plus(createBiasParameters(lstmCellDimension),
-                    (createWeightParameters(lstmCellDimension) * input), "LinearCombinationPlusBias");
+            Parameter H = new Parameter((NDShape) new[] { lstmCellDimension * 4, lstmOutputDimension }, dataType, CNTKLib.GlorotUniformInitializer(1.0, 1, 0, seed++));
+            Variable linearCombinationPrevOutput = CNTKLib.Times(H, prevOutput, "linearCombinationPreviousOutput");
 
-            Func<Variable, Variable> PrevOutputLinearCombination = (previousOutput) =>
-                CNTKLib.Times(createWeightParameters(lstmCellDimension), previousOutput);
+            Variable gateInput = CNTKLib.Plus(linearCombinationPlusBias, linearCombinationPrevOutput, "gateInput");
+            Variable forgetProjection = 
+                CNTKLib.Slice(gateInput, new AxisVector() { new Axis(0) }, new IntVector() { lstmCellDimension * 0 }, new IntVector() { lstmCellDimension * 1 });
+            Variable inputProjection =
+                CNTKLib.Slice(gateInput, new AxisVector() { new Axis(0) }, new IntVector() { lstmCellDimension * 1 }, new IntVector() { lstmCellDimension * 2 });
+            Variable outputProjection =
+                CNTKLib.Slice(gateInput, new AxisVector() { new Axis(0) }, new IntVector() { lstmCellDimension * 2 }, new IntVector() { lstmCellDimension * 3 });
+            Variable candidateProjection =
+                CNTKLib.Slice(gateInput, new AxisVector() { new Axis(0) }, new IntVector() { lstmCellDimension * 3 }, new IntVector() { lstmCellDimension * 4 });
 
-            //  Forget Gate
-            Function ft = 
-                CNTKLib.Sigmoid(
-                    InputLinearCombinationPlusBias() + PrevOutputLinearCombination(stabilizedPrevOutput),
-                    "ForgetGate");
+            Function ft = CNTKLib.Sigmoid(forgetProjection, "ForgetGate");
+            Function it = CNTKLib.Sigmoid(inputProjection, "InputGate");
+            Function ot = CNTKLib.Sigmoid(outputProjection, "OutputGate");
+            Function ctt = CNTKLib.Tanh(candidateProjection, "Candidate");
 
-            //  Input Gate
-            Function it =
-                CNTKLib.Sigmoid(
-                    InputLinearCombinationPlusBias() + PrevOutputLinearCombination(stabilizedPrevOutput),
-                    "InputGate");
-            Function ctt =
-                CNTKLib.Tanh(
-                    InputLinearCombinationPlusBias() + PrevOutputLinearCombination(stabilizedPrevOutput),
-                    "CandidateValue");
+            Function bft = CNTKLib.ElementTimes(prevCellState, ft);
+            Function bit = CNTKLib.ElementTimes(it, ctt);
+            Function ct = CNTKLib.Plus(bft, bit, "CellState");
 
-            //  New Cell State
-            Function ct =
-                CNTKLib.Plus(CNTKLib.ElementTimes(ft, prevCellState), CNTKLib.ElementTimes(it, ctt));
-
-            //  Output Gate
-            Function ot =
-                CNTKLib.Sigmoid(
-                    InputLinearCombinationPlusBias() + PrevOutputLinearCombination(stabilizedPrevOutput),
-                    "OutputGate");
-            Function ht =
-                CNTKLib.ElementTimes(ot, CNTKLib.Tanh(ct), "Output");
-            Function stabilizedHt;
-            if (enableSelfStabilization)
-                stabilizedHt = Stabilize.Build<TElementType>(ht, device, "OutputStabilized");
-            else
-                stabilizedHt = ht;
-
-            //  Prepare output
+            Function h = CNTKLib.ElementTimes(ot, CNTKLib.Tanh(ct), "Output");
             Function c = ct;
-            Function h = (lstmOutputDimension != lstmCellDimension)
-                ? CNTKLib.Times(createWeightParameters(lstmOutputDimension), stabilizedHt, "Output")
-                : stabilizedHt;
-
+            if (lstmOutputDimension != lstmCellDimension)
+            {
+                Parameter P = new Parameter((NDShape) new[] { lstmOutputDimension, lstmCellDimension }, dataType, CNTKLib.GlorotUniformInitializer(1.0, 1, 0, seed++));
+                h = CNTKLib.Times(P, h, "StandarizedOutput");
+            }
+            
             return new Tuple<Function, Function>(h, c);
         }
-
-//        LSTM with Peephole Implementation in CNTK Example
-//        public static Tuple<Function, Function> LSTMPCell<ElementType>(
-//            Variable input, Variable prevOutput, Variable prevCellState, bool enableSelfStabilization, DeviceDescriptor device)
-//        {
-//            int outputDimension = prevOutput.Shape[0];
-//            int cellDimension = prevCellState.Shape[0];
-//
-//            bool isFloatType = typeof(ElementType) == typeof(float);
-//            DataType dataType = isFloatType ? DataType.Float : DataType.Double;
-//
-//            Func<int, Parameter> createBiasParam;
-//            if (isFloatType)
-//                createBiasParam = (dimension) => new Parameter(new int[] { dimension }, 0.01f, device, "BiasParameter");
-//            else
-//                createBiasParam = (dimension) => new Parameter(new int[] { dimension }, 0.01, device, "BiasParameter");
-//
-//            uint seed2 = 1;
-//            Func<int, Parameter> createProjectionParameter = (oDimension) => new Parameter(new int[] { oDimension, NDShape.InferredDimension },
-//                dataType, CNTKLib.GlorotUniformInitializer(1.0, 1, 0, seed2++), device, "WeightMatrix");
-//            Func<int, Parameter> createDiagWeightParameter = (dimension) => new Parameter(new int[] { dimension },
-//                dataType, CNTKLib.GlorotUniformInitializer(1.0, 1, 0, seed2++), device, "StateWeightMatrix");
-//
-//            Function stabilizedPrevOutput;
-//            Function stabilizedPrevCellState;
-//            if (enableSelfStabilization)
-//            {
-//                stabilizedPrevOutput = Stabilize.Build<ElementType>(prevOutput, device, "StabilizedPrevOutput");
-//                stabilizedPrevCellState = Stabilize.Build<ElementType>(prevCellState, device, "StabilizedPrevCellState");
-//            }
-//            else
-//            {
-//                stabilizedPrevOutput = prevOutput;
-//                stabilizedPrevCellState = prevCellState;
-//            }
-//
-//            Func<Variable> projectInput = () =>
-//                CNTKLib.Plus(createBiasParam(cellDimension), (createProjectionParameter(cellDimension) * input), "LinearCombinationPlusBias");
-//
-//            //  Input gate
-//            Function it =
-//                CNTKLib.Sigmoid(
-//                    (Variable)(projectInput() + (createProjectionParameter(cellDimension) * stabilizedPrevOutput)) +
-//                    CNTKLib.ElementTimes(createDiagWeightParameter(cellDimension), stabilizedPrevCellState), "InputGate");
-//            Function bit = CNTKLib.ElementTimes(
-//                it,
-//                CNTKLib.Tanh(projectInput() + (createProjectionParameter(cellDimension) * stabilizedPrevOutput)), "CandidateValue");
-//
-//            //  Forget-me-not gate
-//            Function ft = CNTKLib.Sigmoid(
-//                (Variable)(projectInput() + (createProjectionParameter(cellDimension) * stabilizedPrevOutput)) +
-//                CNTKLib.ElementTimes(createDiagWeightParameter(cellDimension), stabilizedPrevCellState), "ForgetGate");
-//            Function bft = CNTKLib.ElementTimes(ft, prevCellState, "forgetLastCellState");
-//
-//            //  Calculating new cell state
-//            Function ct = CNTKLib.Plus(bft, bit, "CellState");
-//            Function stabilizedCt;
-//            if (enableSelfStabilization)
-//                stabilizedCt = Stabilize.Build<ElementType>(ct, device, "CellStateStablized");
-//            else
-//                stabilizedCt = ct;
-//
-//            //  Output gate
-//            Function ot = CNTKLib.Sigmoid(
-//                (Variable)(projectInput() + (createProjectionParameter(cellDimension) * stabilizedPrevOutput)) +
-//                CNTKLib.ElementTimes(createDiagWeightParameter(cellDimension), stabilizedCt), "OutputGate");
-//            Function ht = CNTKLib.ElementTimes(ot, CNTKLib.Tanh(ct), "Output");
-//            Function stabilizedHt;
-//            if (enableSelfStabilization)
-//                stabilizedHt = Stabilize.Build<ElementType>(ht, device, "OutputStabilized");
-//            else
-//                stabilizedHt = ht;
-//
-//            //  Prepare output
-//            Function c = ct;
-//            Function h = (outputDimension != cellDimension)
-//                ? CNTKLib.Times(createProjectionParameter(outputDimension), stabilizedHt, "Output")
-//                : stabilizedHt;
-//
-//            return new Tuple<Function, Function>(h, c);
-//        }
-//
-//        
-//        private static Tuple<Function, Function> LSTMPComponent<ElementType>(
-//            Variable input,
-//            NDShape outputShape, NDShape cellShape,
-//            Func<Variable, Function> recurrenceHookH,
-//            Func<Variable, Function> recurrenceHookC,
-//            bool enableSelfStabilization,
-//            DeviceDescriptor device)
-//        {
-//            var dh = Variable.PlaceholderVariable(outputShape, input.DynamicAxes);
-//            var dc = Variable.PlaceholderVariable(cellShape, input.DynamicAxes);
-//
-//            var lstmCell = LSTMPCell<ElementType>(input, dh, dc, enableSelfStabilization, device);
-//            var actualDh = recurrenceHookH(lstmCell.Item1);
-//            var actualDc = recurrenceHookC(lstmCell.Item2);
-//
-//            (lstmCell.Item1).ReplacePlaceholders(new Dictionary<Variable, Variable> { { dh, actualDh }, { dc, actualDc } });
-//            return new Tuple<Function, Function>(lstmCell.Item1, lstmCell.Item2);
-//        }
 
         private static Tuple<Function, Function> LSTMComponent<TElementType>(Variable input, NDShape outputShape,
             NDShape cellShape, Func<Variable, Function> recurrenceHookH, Func<Variable, Function> recurrenceHookC,
